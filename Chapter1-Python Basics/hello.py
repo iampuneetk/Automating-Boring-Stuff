1313cdb77108c8cb30f0b1334991f51905536c39
iampuneetk <47330141+iampuneetk@users.noreply.github.com>
2019-02-04 23:37:02 +0530

    Add files via upload

---
+++ Chapter1-Python Basics/hello.py
@@ -0,0 +1,11 @@
+#this programs sayshello and ask for my name
+
+print ("Hello World!")
+print('What IS your name?')
+myName=input()
+print ("Its good to meet you, " + myName)
+print("The Length of your name is ")
+print(len(myName))
+print('What is your Age?')
+myAge =input()
+print("You will be " +str(int(myAge) +1 )+ " in a year!")
