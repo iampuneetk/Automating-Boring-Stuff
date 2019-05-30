10e357bc8a348149c8937f550470b50f1475c913
iampuneetk <47330141+iampuneetk@users.noreply.github.com>
2019-05-31 00:52:21 +0530

    Add files via upload

---
+++ Chapter-2-Flow control/flowcontrol.py
@@ -0,0 +1,13 @@
+'''
+    Program for Learning Automation with Python
+    Date : 05/02/2019
+    Author : iampuneetk
+'''
+spam=input('Enter Value of spam\n')
+
+if(spam=='1'):
+    print('Hello')
+elif(spam=='2'):
+    print('Howdy')
+else:
+    print('Greetings')
---
+++ Chapter-2-Flow control/flowcontrol1.py
@@ -0,0 +1,10 @@
+'''
+    Program for Learning Automation with Python
+    Date : 05/02/2019
+    Author : iampuneetk
+'''
+password="thewinningsword"
+if password == 'swordfish':
+    print('Access Granted')
+else: 
+    print('Wrong Password')
